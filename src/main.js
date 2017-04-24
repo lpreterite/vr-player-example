1a9fec2d9fc6af7687a67f96a5eb0c75b042d0c4
packy <lpreterite@126.com>
2017-04-24 17:48:05 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var player = jwplayer('play').setup({
+    playlist: [{
+        title: 'AgqYcfAT',
+        mediaid: 'AgqYcfAT',
+        stereomode: 'monoscopic',
+        file: '//vr.jwplayer.com/content/AgqYcfAT/AgqYcfAT-8yQ1cYbs.mp4'
+    }]
+});
