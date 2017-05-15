9f089427f2b04f238b36ee382d051d55e8f13187
packy <lpreterite@126.com>
2017-05-15 23:36:47 +0800

    add aframe vr vadio example

--- src/main.js
+++ src/main.js
@@ -1,8 +1,1 @@
-var player = jwplayer('play').setup({
-    playlist: [{
-        title: 'AgqYcfAT',
-        mediaid: 'AgqYcfAT',
-        stereomode: 'monoscopic',
-        file: '//vr.jwplayer.com/content/AgqYcfAT/AgqYcfAT-8yQ1cYbs.mp4'
-    }]
-});
+require('aframe');
