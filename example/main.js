702f32defd2210a446456b4ea5e75bc46e3aa17a
packy <lpreterite@126.com>
2017-05-16 14:16:19 +0800

    add webpack and aframe example

--- example/main.js
+++ example/main.js
@@ -1,15 +1,15 @@
-//require('aframe');
+require('aframe');
 
 const $play = document.querySelector('#play');
 const $video = document.querySelector('#video');
 
 var pause = false;
 $play.addEventListener('click', function(){
-	if(!pause){
-		$video.pause();
-		pause = true;
-	}else{
-		$video.play();
-		pause = false;
-	}
+    if(!pause){
+        $video.pause();
+        pause = true;
+    }else{
+        $video.play();
+        pause = false;
+    }
 });
